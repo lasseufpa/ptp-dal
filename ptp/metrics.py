90bbadbf92b1d971f29f27faff0f3c57ee0efa02
Igor Freire <igor.auad@gmail.com>
2019-05-18 19:33:24 -0300

    Avoid code repetition on metrics plots
    Instead of repeating the code to plot every single metric, iterate over a list
    of keys and plot them with their respective labels.

--- ptp/metrics.py
+++ ptp/metrics.py
@@ -6,6 +6,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+pkts_keys   = ["pkts_average",
+               "pkts_ewma",
+               "pkts_median",
+               "pkts_min"]
+pkts_labels = ["Sample-average",
+               "EWMA",
+               "Sample-median",
+               "EAPF"]
+ls_keys     = ["ls_t2",
+               "ls_t1",
+               "ls_eff"]
+ls_labels   = ["LSE (t2)",
+               "LSE (t1)",
+               "LSE"]
+
+
 class Analyser():
     def __init__(self, data):
         """PTP metrics analyser
@@ -93,22 +109,14 @@ class Analyser():
 
         # Least-squares estimations
         if (show_ls):
-            i_ls_t2  = [r["idx"] for r in self.data if "x_ls_t2" in r]
-            i_ls_t1  = [r["idx"] for r in self.data if "x_ls_t1" in r]
-            i_ls_eff = [r["idx"] for r in self.data if "x_ls_eff" in r]
-            x_ls_t2  = [r["x_ls_t2"] for r in self.data if "x_ls_t2" in r]
-            x_ls_t1  = [r["x_ls_t1"] for r in self.data if "x_ls_t1" in r]
-            x_ls_eff = [r["x_ls_eff"] for r in self.data if "x_ls_eff" in r]
+            for i, suffix in enumerate(ls_keys):
+                key  = "x_" + suffix
+                i_ls = [r["idx"] for r in self.data if key in r]
+                x_ls = [r[key] for r in self.data if key in r]
 
-            if (len(x_ls_t1) > 0):
-                plt.scatter(i_ls_t1, x_ls_t1,
-                            label="LSE (t1)", marker="x", s=1.0)
-            if (len(x_ls_t2) > 0):
-                plt.scatter(i_ls_t2, x_ls_t2,
-                            label="LSE (t2)", marker="x", s=1.0)
-            if (len(x_ls_eff) > 0):
-                plt.scatter(i_ls_eff, x_ls_eff,
-                            label="LSE", marker="x", s=1.0)
+                if (len(x_ls) > 0):
+                    plt.scatter(i_ls, x_ls,
+                                label=ls_labels[i], marker="x", s=1.0)
 
         # Kalman filtering output
         if (show_kf):
@@ -123,33 +131,14 @@ class Analyser():
 
         # Packet Selection estimation
         if (show_pkts):
-            i_pkt_avg     = [r["idx"] for r in self.data if "x_pkts_average" in r]
-            i_pkt_ewma    = [r["idx"] for r in self.data if "x_pkts_ewma" in r]
-            i_pkt_median  = [r["idx"] for r in self.data if "x_pkts_median" in r]
-            i_pkt_minimum = [r["idx"] for r in self.data if "x_pkts_min" in r]
-            x_pkt_avg     = [r["x_pkts_average"] for r in self.data if
-                             "x_pkts_average" in r]
-            x_pkt_ewma    = [r["x_pkts_ewma"] for r in self.data if
-                             "x_pkts_ewma" in r]
-            x_pkt_median  = [r["x_pkts_median"] for r in self.data if
-                             "x_pkts_median" in r]
-            x_pkt_minimum = [r["x_pkts_min"] for r in self.data if "x_pkts_min" in r]
+            for i, suffix in enumerate(pkts_keys):
+                key    = "x_" + suffix
+                i_pkts = [r["idx"] for r in self.data if key in r]
+                x_pkts = [r[key] for r in self.data if key in r]
 
-            if (len(x_pkt_avg) > 0):
-                plt.scatter(i_pkt_avg, x_pkt_avg,
-                            label="Sample-average", marker="v", s=1.0)
-
-            if (len(x_pkt_ewma) > 0):
-                plt.scatter(i_pkt_ewma, x_pkt_ewma,
-                            label="EWMA", marker="v", s=1.0)
-
-            if (len(x_pkt_median) > 0):
-                plt.scatter(i_pkt_median, x_pkt_median,
-                            label="Sample-median", marker="v", s=1.0)
-
-            if (len(x_pkt_minimum) > 0):
-                plt.scatter(i_pkt_minimum, x_pkt_minimum,
-                            label="EAPF", marker="v", s=1.0)
+                if (len(x_pkts) > 0):
+                    plt.scatter(i_pkts, x_pkts,
+                                label=pkts_labels[i], marker="v", s=1.0)
 
         # Best raw measurements
         if (show_best):
@@ -194,25 +183,14 @@ class Analyser():
 
         # Least-squares estimations
         if (show_ls):
-            i_ls_t2      = [r["idx"] for r in self.data if "x_ls_t2" in r]
-            i_ls_t1      = [r["idx"] for r in self.data if "x_ls_t1" in r]
-            i_ls_eff     = [r["idx"] for r in self.data if "x_ls_eff" in r]
-            x_ls_err_t2  = [r["x_ls_t2"] - r["x"] for r in self.data
-                            if "x_ls_t2" in r]
-            x_ls_err_t1  = [r["x_ls_t1"] - r["x"] for r in self.data
-                            if "x_ls_t1" in r]
-            x_ls_err_eff = [r["x_ls_eff"] - r["x"] for r in self.data
-                            if "x_ls_eff" in r]
+            for i, suffix in enumerate(ls_keys):
+                key  = "x_" + suffix
+                i_ls = [r["idx"] for r in self.data if key in r]
+                x_ls = [r[key] - r["x"] for r in self.data if key in r]
 
-            if (len(x_ls_err_t1) > 0):
-                plt.scatter(i_ls_t1, x_ls_err_t1,
-                            label="LSE (t1)", marker="x", s=1.0)
-            if (len(x_ls_err_t2) > 0):
-                plt.scatter(i_ls_t2, x_ls_err_t2,
-                            label="LSE (t2)", marker="x", s=1.0)
-            if (len(x_ls_err_eff) > 0):
-                plt.scatter(i_ls_eff, x_ls_err_eff,
-                            label="LSE", marker="x", s=1.0)
+                if (len(x_ls) > 0):
+                    plt.scatter(i_ls, x_ls,
+                                label=ls_labels[i], marker="x", s=1.0)
 
         # Kalman filtering output
         if (show_kf):
@@ -223,34 +201,14 @@ class Analyser():
 
         # Packet Selection estimations
         if (show_pkts):
-            i_pkt_avg         = [r["idx"] for r in self.data if "x_pkts_average" in r]
-            i_pkt_ewma        = [r["idx"] for r in self.data if "x_pkts_ewma" in r]
-            i_pkt_median      = [r["idx"] for r in self.data if "x_pkts_median" in r]
-            i_pkt_minimum     = [r["idx"] for r in self.data if "x_pkts_min" in r]
-            x_pkt_err_avg     = [r["x_pkts_average"] - r["x"] for r in
-                                 self.data if "x_pkts_average" in r]
-            x_pkt_err_ewma    = [r["x_pkts_ewma"] - r["x"] for r in self.data
-                                 if "x_pkts_ewma" in r]
-            x_pkt_err_median  = [r["x_pkts_median"] - r["x"] for r in self.data
-                                 if "x_pkts_median" in r]
-            x_pkt_err_minimum = [r["x_pkts_min"] - r["x"] for r in self.data if
-                                 "x_pkts_min" in r]
+            for i, suffix in enumerate(pkts_keys):
+                key    = "x_" + suffix
+                i_pkts = [r["idx"] for r in self.data if key in r]
+                x_pkts = [r[key] - r["x"] for r in self.data if key in r]
 
-            if (len(x_pkt_err_avg) > 0):
-                plt.scatter(i_pkt_avg, x_pkt_err_avg,
-                            label="Sample-average", marker="v", s=1.0)
-
-            if (len(x_pkt_err_ewma) > 0):
-                plt.scatter(i_pkt_ewma, x_pkt_err_ewma,
-                            label="EWMA", marker="v", s=1.0)
-
-            if (len(x_pkt_err_median) > 0):
-                plt.scatter(i_pkt_median, x_pkt_err_median,
-                            label="Sample-median", marker="v", s=1.0)
-
-            if (len(x_pkt_err_minimum) > 0):
-                plt.scatter(i_pkt_minimum, x_pkt_err_minimum,
-                            label="EAPF", marker="v", s=1.0)
+                if (len(x_pkts) > 0):
+                    plt.scatter(i_pkts, x_pkts,
+                                label=pkts_labels[i], marker="v", s=1.0)
 
         plt.xlabel('Realization')
         plt.ylabel('Time offset Error (ns)')
@@ -332,22 +290,14 @@ class Analyser():
 
         # Show least-squares estimations
         if (show_ls):
-            i_ls_t2  = [r["idx"] for r in self.data if "y_ls_t2" in r]
-            i_ls_t1  = [r["idx"] for r in self.data if "y_ls_t1" in r]
-            i_ls_eff = [r["idx"] for r in self.data if "y_ls_eff" in r]
-            y_ls_t2  = [1e9*r["y_ls_t2"] for r in self.data if "y_ls_t2" in r]
-            y_ls_t1  = [1e9*r["y_ls_t1"] for r in self.data if "y_ls_t1" in r]
-            y_ls_eff = [1e9*r["y_ls_eff"] for r in self.data if "y_ls_eff" in r]
+            for i, suffix in enumerate(ls_keys):
+                key  = "y_" + suffix
+                i_ls = [r["idx"] for r in self.data if key in r]
+                y_ls = [1e9*r[key] for r in self.data if key in r]
 
-            if (len(y_ls_t2) > 0):
-                plt.scatter(i_ls_t2, y_ls_t2,
-                            label="LSE (t2)", marker="x", s=1.0)
-            if (len(y_ls_t1) > 0):
-                plt.scatter(i_ls_t1, y_ls_t1,
-                            label="LSE (t1)", marker="x", s=1.0)
-            if (len(y_ls_eff) > 0):
-                plt.scatter(i_ls_eff, y_ls_eff,
-                            label="LSE", marker="x", s=1.0)
+                if (len(y_ls) > 0):
+                    plt.scatter(i_ls, y_ls,
+                                label=ls_labels[i], marker="x", s=1.0)
 
         # Kalman filtering output
         if (show_kf):
@@ -447,67 +397,31 @@ class Analyser():
 
         # Least-squares estimations
         if (show_ls):
-            i_ls_t2      = [r["idx"] for r in self.data if "x_ls_t2" in r]
-            i_ls_t1      = [r["idx"] for r in self.data if "x_ls_t1" in r]
-            i_ls_eff     = [r["idx"] for r in self.data if "x_ls_eff" in r]
-            x_ls_err_t2  = [r["x_ls_t2"] - r["x"] for r in self.data
-                            if "x_ls_t2" in r]
-            x_ls_err_t1  = [r["x_ls_t1"] - r["x"] for r in self.data
-                            if "x_ls_t1" in r]
-            x_ls_err_eff = [r["x_ls_eff"] - r["x"] for r in self.data
-                            if "x_ls_eff" in r]
+            for i, suffix in enumerate(ls_keys):
+                key  = "x_" + suffix
+                i_ls = [r["idx"] for r in self.data if key in r]
+                x_ls = [r[key] - r["x"] for r in self.data if key in r]
 
-            if (len(x_ls_err_t1) > 0):
-                tau_ls_t1, mtie_ls_t1 = self.mtie(x_ls_err_t1)
-                plt.scatter(tau_ls_t1, mtie_ls_t1,
-                            label="LSE (t1)", marker="x", s=80.0, alpha=1)
-            if (len(x_ls_err_t2) > 0):
-                tau_ls_t2, mtie_ls_t2 = self.mtie(x_ls_err_t2)
-                plt.scatter(tau_ls_t2, mtie_ls_t2,
-                            label="LSE (t2)", marker="x", s=80.0, alpha=1)
-            if (len(x_ls_err_eff) > 0):
-                tau_ls_eff, mtie_ls_eff = self.mtie(x_ls_err_eff)
-                plt.scatter(tau_ls_eff, mtie_ls_eff,
-                            label="LSE", marker="x", s=80.0, alpha=1)
+                if (len(x_ls) > 0):
+                    tau_ls, mtie_ls = self.mtie(x_ls)
+                    plt.scatter(tau_ls, mtie_ls,
+                            label=ls_labels[i], marker="x", s=80.0, alpha=1)
 
         # Packet Selection estimations
         if (show_pkts):
             # EWMA and the recursive moving average have transitories. Try to
             # skip it by throwing away an arbitrary number of initial values.
             post_tran_data    = self.data[200:]
-            i_pkts_avg        = [r["idx"] for r in post_tran_data
-                                 if "x_pkts_average" in r]
-            i_pkts_ewma       = [r["idx"] for r in post_tran_data
-                                 if "x_pkts_ewma" in r]
-            i_pkts_median     = [r["idx"] for r in self.data
-                                 if "x_pkts_median" in r]
-            i_pkts_minimum    = [r["idx"] for r in self.data
-                                 if "x_pkts_min" in r]
-            x_pkts_err_avg    = [r["x_pkts_average"] - r["x"] for r
-                                 in post_tran_data if "x_pkts_average" in r]
-            x_pkts_err_ewma   = [r["x_pkts_ewma"] - r["x"] for r
-                                 in post_tran_data if"x_pkts_ewma" in r]
-            x_pkts_err_median = [r["x_pkts_median"] - r["x"] for r in self.data
-                                 if "x_pkts_median" in r]
-            x_pkts_err_min    = [r["x_pkts_min"] - r["x"] for r in self.data
-                                 if "x_pkts_min" in r]
+            for i, suffix in enumerate(pkts_keys):
+                key    = "x_" + suffix
+                i_pkts = [r["idx"] for r in post_tran_data if key in r]
+                x_pkts = [r[key] - r["x"] for r in post_tran_data if key in r]
 
-            if (len(x_pkts_err_avg) > 0):
-                tau_pkts_avg, mtie_pkts_avg = self.mtie(x_pkts_err_avg)
-                plt.scatter(tau_pkts_avg, mtie_pkts_avg,
-                            label="Sample-average", marker="v", s=80.0, alpha=0.7)
-            if (len(x_pkts_err_ewma) > 0):
-                tau_pkts_ewma, mtie_pkts_ewma = self.mtie(x_pkts_err_ewma)
-                plt.scatter(tau_pkts_ewma, mtie_pkts_ewma,
-                            label="EWMA", marker="v", s=80.0, alpha=0.7)
-            if (len(x_pkts_err_median) > 0):
-                tau_pkts_median, mtie_pkts_median = self.mtie(x_pkts_err_median)
-                plt.scatter(tau_pkts_median, mtie_pkts_median,
-                            label="Sample-median", marker="v", s=80.0, alpha=0.7)
-            if (len(x_pkts_err_min) > 0):
-                tau_pkts_min, mtie_pkts_min = self.mtie(x_pkts_err_min)
-                plt.scatter(tau_pkts_min, mtie_pkts_min,
-                            label="EAPF", marker="v", s=80.0, alpha=0.7)
+                if (len(x_pkts) > 0):
+                    tau_pkts, mtie_pkts = self.mtie(x_pkts)
+                    plt.scatter(tau_pkts, mtie_pkts,
+                                label=pkts_labels[i], marker="v", s=80.0,
+                                alpha=0.7)
 
         # Kalman filtering output
         if (show_kf):
